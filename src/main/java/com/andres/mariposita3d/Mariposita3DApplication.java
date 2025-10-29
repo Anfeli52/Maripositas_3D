baf927d033c56aa2bf8099bb0afd8986faa0d7d4
Andrés <anfeli201111@gmail.com>
2025-10-29 15:04:18 -0500

    Primer Commit

---
+++ src/main/java/com/andres/mariposita3d/Mariposita3DApplication.java
@@ -0,0 +1,13 @@
+package com.andres.mariposita3d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mariposita3DApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mariposita3DApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andres/mariposita3d/Mariposita3DApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.mariposita3d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mariposita3DApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
