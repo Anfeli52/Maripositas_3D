adc89405d4a07fd159755f79eedb4485ca4719d3
andresdiazds3 <cesar_and.diaz@uao.edu.co>
2025-11-05 16:26:39 -0500

    Capas de coleccion Usuario

---
+++ src/main/java/com/andres/mariposita3d/Collection/Usuario.java
@@ -0,0 +1,77 @@
+package com.andres.mariposita3d.Collection;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document("usuarios")
+public class Usuario {
+
+    @Id
+    private String id;
+
+    @Field("nombre")
+    private String nombre;
+
+    @Field("correo")
+    private String correo;
+
+    @Field("rol")
+    private String rol;
+
+    @Field("contrasena")
+    private String contrasena;
+
+    @Field("fecha_registro")
+    private Date fechaRegistro;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+}
---
+++ src/main/java/com/andres/mariposita3d/Service/IUsuarioService.java
@@ -0,0 +1,18 @@
+// IUsuarioService.java
+package com.andres.mariposita3d.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.andres.mariposita3d.Collection.Usuario;
+
+public interface IUsuarioService {
+
+    public List<Usuario> all();
+
+    public Optional<Usuario> findById(String id);
+
+    public Usuario save(Usuario usuario);
+
+    public void delete(String id);
+}
